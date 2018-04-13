9f0aae2a5eee8ecd1b30763e29589b68d714d292
Kevin Flores <34111148+kflores097@users.noreply.github.com>
2018-04-12 21:26:52 -0400

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -7,7 +7,7 @@ public class MazeSolver{
 
     private Maze maze;
     
-    private  boolean success;
+    private  boolean success; //determine if explorer can reach treasure (if any)
 
     public MazeSolver(Maze maze1){
 	maze = maze1;
@@ -19,22 +19,29 @@ public class MazeSolver{
 
     private boolean solveThis(){
 
-	if (maze.explorerIsOnA() == maze.TREASURE) return true;
-        if (maze.explorerIsOnA() == maze.WALL) return false;
-	else return true;
+	if (maze.explorerIsOnA() == maze.TREASURE) return true; //base case
+        if (maze.explorerIsOnA() == maze.WALL) return false; //other base case
+	else{
+		int[] directions = {maze.EAST, maze.NORTH, maze.WEST, maze.SOUTH}; 
+	    for (int nextStep: directions){ 
 
-	    
-	
+	       
+	         Maze mazeBeforeNextStep = new Maze(maze);
+		 maze.dropA(maze.WALL);
+
+		 maze.go(nextStep);
+		 System.out.println(maze);
+
+		 if (solveThis()) return true;
+
+		
+
+		 maze = mazeBeforeNextStep;
+	    }
+	    return false;
+	}
     }
-
-    public String toString(){
-	return "statement of prob is " + success;
-    }
-}
-						    
-    
-    
-
-
-    
-
+	public String toString(){
+		return "boolean of the statement is " + success;
+	}
+}	
